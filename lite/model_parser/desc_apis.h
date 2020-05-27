f6f49855f726ea682cabcacbeb1b88a8a99be0bf
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2020-05-27 07:53:08 +0000

    update

--- lite/model_parser/desc_apis.h
+++ lite/model_parser/desc_apis.h
@@ -32,32 +32,32 @@ class VarDescAPI {
   enum class Type {
     // Pod Types
     BOOL = 0,
-    INT16,
-    INT32,
-    INT64,
-    FP16,
-    FP32,
-    FP64,
+    INT16 = 1,
+    INT32 = 2,
+    INT64 = 3,
+    FP16 = 4,
+    FP32 = 5,
+    FP64 = 6,
     // Tensor<size_t> is used in C++.
-    SIZE_T,
-    UINT8,
-    INT8,
+    SIZE_T = 19,
+    UINT8 = 20,
+    INT8 = 21,
 
     // Other types that may need additional descriptions
-    LOD_TENSOR,
-    SELECTED_ROWS,
-    FEED_MINIBATCH,
-    FETCH_LIST,
-    STEP_SCOPES,
-    LOD_RANK_TABLE,
-    LOD_TENSOR_ARRAY,
-    PLACE_LIST,
-    READER,
+    LOD_TENSOR = 7,
+    SELECTED_ROWS = 8,
+    FEED_MINIBATCH = 9,
+    FETCH_LIST = 10,
+    STEP_SCOPES = 11,
+    LOD_RANK_TABLE = 12,
+    LOD_TENSOR_ARRAY = 13,
+    PLACE_LIST = 14,
+    READER = 15,
     // Any runtime decided variable type is raw
     // raw variables should manage their own allocations
     // in operators like nccl_op
-    RAW,
-    TUPLE
+    RAW = 17,
+    TUPLE = 18,
   };
 
   using VarDataType = Type;
--- lite/model_parser/parser_test.cc
+++ lite/model_parser/parser_test.cc
@@ -1,7 +1,44 @@
 #include "lite/model_parser/model_parser.h"
+#include "lite/model_parser/pb/program_desc.h"
+#include "lite/model_parser/pb/var_desc.h"
+#include <google/protobuf/text_format.h>
+#include <chrono>  // NOLINT
+
+// Timer for timer
+class Timer {
+ public:
+  std::chrono::high_resolution_clock::time_point start;
+  std::chrono::high_resolution_clock::time_point startu;
+
+  void tic() { start = std::chrono::high_resolution_clock::now(); }
+  double toc() {
+    startu = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_span =
+        std::chrono::duration_cast<std::chrono::duration<double>>(startu -
+                                                                  start);
+    double used_time_ms = static_cast<double>(time_span.count()) * 1000.0;
+    return used_time_ms;
+  }
+};
 
 int main() {
-  paddle::lite::cpp::ProgramDesc prog;
-  paddle::lite::LoadModelFbs("/shixiaowei02/Paddle-Lite-FlatBuf/framework_test/save_model.bin", &prog);
+  paddle::lite::cpp::ProgramDesc cpp_prog;
+  double t = 0;
+
+  Timer timer;
+  timer.tic();
+  paddle::lite::LoadModelFbs("/shixiaowei02/Paddle-Lite-FlatBuf/framework_test/save_model.bin", &cpp_prog);
+  t = timer.toc();
+  std::cout << "time = " << t << std::endl;
+
+/*
+  paddle::framework::proto::ProgramDesc pb_proto_prog;
+  paddle::lite::pb::ProgramDesc pb_prog(&pb_proto_prog);
+  paddle::lite::TransformProgramDescCppToAny(cpp_prog, &pb_prog);
+
+  std::string pb_str;
+  google::protobuf::TextFormat::PrintToString(pb_proto_prog, &pb_str);
+  std::cout << pb_str << std::endl;
+*/
   return 0;
 }
