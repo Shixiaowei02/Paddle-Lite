c5af9ac6a700b9f45ef3436947d659431f7b3087
Shixiaowei02 <shixiaowei02@baidu.com>
2020-06-05 17:33:22 +0800

    update

--- lite/model_parser/flatbuffers/program_desc.h
+++ lite/model_parser/flatbuffers/program_desc.h
@@ -1,1 +1,63 @@
-i
+// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include "lite/model_parser/desc_apis.h"
+#include "lite/model_parser/flatbuffers/framework_generated.h"
+
+namespace paddle {
+namespace lite {
+namespace fbs {
+
+class ProgramDesc : public ProgramDescAPI, public proto::ProgramDescT {
+ public:
+  ProgramDesc() = delete;
+
+  explicit ProgramDesc(proto::ProgramDesc *desc) {}
+
+  size_t BlocksSize() const override {
+    return blocks.size();
+  }
+
+  void ClearBlocks() override {
+    blocks.clear();
+  }
+
+  template <typename T>
+  T *GetBlock(int32_t idx);
+
+  template <typename T>
+  T *AddBlock() {
+
+  }
+
+  bool HasVersion() const override {
+    return version.get();
+  }
+
+  int64_t Version() const override {
+    return version->version;
+  }
+
+  void SetVersion(int64_t version_in) override {
+    version_in = version->version;
+  }
+
+ private:
+
+};
+
+}  // namespace fbs
+}  // namespace lite
+}  // namespace paddle
