c2762d9ccb7926af715def60da9e116d09b9873a
chenjiaoAngel <chenjiaobuaa@126.com>
2020-07-22 16:46:52 +0800

    qa/naive_buffer

---
+++ lite/model_parser/io_test.cc
@@ -0,0 +1,70 @@
+#include "lite/api/paddle_use_kernels.h"
+#include "lite/api/paddle_use_ops.h"
+#include "lite/api/light_api.h"
+#include <assert.h>
+
+class Timer {
+ public:
+  std::chrono::high_resolution_clock::time_point start;
+  std::chrono::high_resolution_clock::time_point startu;
+
+   void tic() { start = std::chrono::high_resolution_clock::now(); }
+  double toc() {
+    startu = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_span =
+        std::chrono::duration_cast<std::chrono::duration<double>>(startu -
+                                                                  start);
+    double used_time_ms = static_cast<double>(time_span.count()) * 1000.0;
+    return used_time_ms;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cout << "[FATAL] Usage: ./test_fbs_io {model_path}" << std::endl;
+  }
+  const std::string path(argv[1]);
+  std::cout << "Model path: " << path << std::endl;
+  {
+    paddle::lite::LightPredictor predictor(path, false);
+  }
+  std::cout << "======= Timer start =======" << std::endl;
+  double t = 0.f;
+  Timer timer;
+  timer.tic();
+  for (size_t i = 0; i < 1000; ++i) {
+    paddle::lite::LightPredictor predictor(path, false);
+  }
+  t = timer.toc();
+  std::cout << "time = " << t / 1000 << std::endl;
+
+  paddle::lite::LightPredictor predictor(path, false);
+  for (auto& name: predictor.GetInputNames()) {
+    std::cout << "input_names: " << name << std::endl;
+  }
+  for (auto& name: predictor.GetOutputNames()) {
+    std::cout << "output_names: " << name << std::endl;
+  }
+  auto* tensor_in = predictor.GetInput(0);
+  const int N = 2;
+  const int C = 3;
+  const int H = 128;
+  const int W = 128;
+
+  tensor_in->Resize({N, C, H, W});
+  float* data = tensor_in->mutable_data<float>();
+  for (size_t i = 0; i < N*C*H*W; ++i) {
+          data[i] = 1.f;
+  }
+
+  predictor.Run();
+
+  auto* tensor_out = predictor.GetOutput(0);
+  auto temp_shape = tensor_out->dims();
+  float const* data_o = tensor_out->data<float>();
+  for (size_t i = 0; i < temp_shape.production(); ++i) {
+         std::cout << "out " << i << ": " << data_o[i] << std::endl;
+  }
+
+  return 0;
+}
