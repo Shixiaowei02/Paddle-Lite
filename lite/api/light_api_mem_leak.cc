8f6e2712a5f43a26b637f2bc8be062095bc2c6b2
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2020-11-13 06:15:44 +0000

    test

--- lite/api/light_api_mem_leak.cc
+++ lite/api/light_api_mem_leak.cc
@@ -11,9 +11,31 @@ int main() {
     const std::string model_path{"/shixiaowei02/Paddle-Lite-v2.7/Paddle-Lite/build_opt/mobilev1_opt_out.nb"};
     {
       paddle::lite::LightPredictor predictor(model_path, false);
-    }
-    if (i % 100 == 0) {
-      HeapProfilerDump("here");
+      paddle::lite::LightPredictor predictor2(model_path, false);
+      paddle::lite::LightPredictor* p;
+      int size = 100;
+
+      for (size_t j = 0; j < 10000; ++j) {
+        if (j % 2 == 0) {
+          p = &predictor;
+          size = 100;
+        } else {
+          p = &predictor2;
+          size = 200;
+        }
+        for (const auto& name: p->GetInputNames()) {
+          auto* input_tensor = p->GetInputByName(name);
+          input_tensor->Resize(paddle::lite::DDim(std::vector<int64_t>({1, 3, size, 100})));
+          auto* data = input_tensor->mutable_data<float>();
+          for (int i = 0; i < 1 * 3 * size * 100; i++) {
+            data[i] = i;
+          }
+        }
+        p->Run();
+        if (j % 15 == 0) {
+          HeapProfilerDump("here2");
+        }
+      }
     }
   }
   HeapProfilerStop();
